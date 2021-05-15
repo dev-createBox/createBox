6617880652b2fa5759b171b1cf1ee34f45359019
JunSeo Kwak <Ruggellia@google.com>
2021-05-15 16:02:17 +0900

    first commit

---
+++ createbox/src/main/java/com/cbox/createbox/CreateboxApplication.java
@@ -0,0 +1,13 @@
+package com.cbox.createbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateboxApplication.class, args);
+	}
+
+}
---
+++ createbox/src/test/java/com/cbox/createbox/CreateboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbox.createbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
