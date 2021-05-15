152ab5e57feb9a804c581eef754e30d3be3fd71d
JunSeo Kwak <Ruggellia@google.com>
2021-05-15 16:50:21 +0900

    마스터 충돌테스트

--- createbox/src/main/java/com/cbox/createbox/CreateboxApplication.java
+++ createbox/src/main/java/com/cbox/createbox/CreateboxApplication.java
@@ -8,7 +8,7 @@ public class CreateboxApplication {
 
 	public static void main(String[] args) {
 		
-		// 브랜치 작업 -> 마스터 합치기 테스트
+		// 브랜치 작업 -> 마스터 합치기 충돌테스트
 		SpringApplication.run(CreateboxApplication.class, args);
 	}
 
