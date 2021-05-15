db26d53029100323db5b88fae98f721b2f1a33d8
JunSeo Kwak <Ruggellia@google.com>
2021-05-15 17:03:13 +0900

    브랜치 충돌커밋

--- createbox/src/main/java/com/cbox/createbox/CreateboxApplication.java
+++ createbox/src/main/java/com/cbox/createbox/CreateboxApplication.java
@@ -9,6 +9,7 @@ public class CreateboxApplication {
 	public static void main(String[] args) {
 		
 		// 브랜치 작업 -> 마스터 합치기 충돌테스트
+		// 로컬 수정 내용
 		SpringApplication.run(CreateboxApplication.class, args);
 	}
 
